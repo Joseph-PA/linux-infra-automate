9a5345ddb9ad640b2698db77c84c849bc2139f8f
Joseph-PA <pa_joseph123@yahoo.co.in>
2015-04-18 08:08:07 +0530

    first commit

---
+++ cpu_memory_df.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import os
+import subprocess 
+print "Choose from the options below:\n";
+print "1. I will print load on server"
+print "2. I will print memory usage on server"
+print "3. I will print df output"
+print "1 or 2 or 3"
+choice = raw_input("Enter Choice:\n")
+choice = int(choice)
+if ( choice == 1 ) :
+    print "Executing w ..."
+    subprocess.call(['w'])
+if ( choice == 2) :
+    print "Executing free ..."
+    subprocess.call(['free', '-m'])
+if ( choice == 3 ) :
+    print "Executing df"
+    subprocess.call(['df', '-h'])
+    
+raw_input ("Press enter to bid adieu")
+print "Bye"
